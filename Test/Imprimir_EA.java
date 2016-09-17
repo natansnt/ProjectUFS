37e69df56bdeb71550bd528c0fb95070a7d5b552
-N a T a N - <-N a T a N ->
2016-09-17 18:02:23 -0300

    Include Test's Classes

---
+++ Test/Imprimir_EA.java
@@ -0,0 +1,34 @@
+package My;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Imprimir_EA {
+
+	public static void main(String[] args) {
+
+		try {
+			Scanner ler = new Scanner(new FileReader(args[0]));
+			int ordem = ler.nextInt();
+			int edge = ler.nextInt();
+			Graph x = new Graph(ordem);
+
+			for (int i = 0; i < edge; i++)
+				x.join(ler.nextInt(), ler.nextInt(), ler.nextInt());
+
+			for (int i = 0; i < ordem; i++) {
+				System.out.print(i + "-> ");
+				while (x.EA[i].hasNext()) {
+					System.out.print(x.EA[i].next() + " ");
+				}
+				System.out.println();
+			}
+
+			ler.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
